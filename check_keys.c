587e1c5ce564b6d61024c4a9f17456bb3351aa40
Abdelilah Ommane <aomman@e2r5p12.1337.ma>
2023-02-13 20:48:40 +0100

    OK

--- check_keys.c
+++ check_keys.c
@@ -12,17 +12,54 @@
 
 #include"cub3d.h"
 
+int	zero_check(t_var *var)
+{
+	if (var->map_elmnt[var->i][var->j] == '0')
+	{
+		if (var->map_elmnt[var->i - 1] == NULL
+			|| var->map_elmnt[var->i - 1][var->j] <= 32)
+			return (1);
+		else if (var->map_elmnt[var->i][var->j - 1] <= 32
+			|| var->map_elmnt[var->i][var->j + 1] <= 32)
+			return (1);
+		else if (var->map_elmnt[var->i + 1] == NULL
+			|| var->map_elmnt[var->i + 1][var->j] <= 32
+			|| var->map_elmnt[var->i + 1][var->j] == '\0')
+			return (1);
+	}
+	return (0);
+}
+
+int	ft_check(t_var *var)
+{
+	while (var->map_elmnt[var->i])
+	{
+		var->j = 0;
+		while (var->map_elmnt[var->i][var->j])
+		{
+			if (var->map_elmnt[var->i][var->j] >= 48)
+			{
+				if (zero_check(var) == 1)
+					return (1);
+			}
+			var->j++;
+		}
+		var->i++;
+	}
+	return (0);
+}
+
 int	check_map_component(t_var *var)
 {
+	int	space;
+
+	space = 0;
 	var->i = 0;
 	if (var->checker == 6)
 	{
 		var->i = var->checker;
-		while (var->map_elmnt[var->i])
-		{
-			printf("%s\n", var->map_elmnt[var->i]);
-			var->i++;
-		}
+		if (ft_check(var) == 1)
+			return (1);
 	}
 	else
 		return (1);
